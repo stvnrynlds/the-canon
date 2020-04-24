0372b86f8788fb34f8a6a35d7d23c1a362eedb7e
Steven Reynolds <hi@stevenreynolds.me>
2020-04-24 16:11:39 -0500

    Add Old & New Testament data

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,22 @@
 <script>
 	export let name;
+
+	import { OLD_TESTAMENT, NEW_TESTAMENT } from "./data";
+	import { onMount } from "svelte";
+	
 </script>
 
 <main>
+<h1>The Canons</h1>
+	<h2>Old Testament</h2>
+	{#each OLD_TESTAMENT as book}
+		<li>{book.name}</li>
+	{/each}
+	<h2>New NEW_TESTAMENT</h2>
+	{#each NEW_TESTAMENT as book}
+		<li>{book.name}</li>
+	{/each}
+	<button>Show Only Old Testament</button>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
@@ -11,13 +25,12 @@
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #EE4A3F;
+		font-family: "Work Sans";
 		font-size: 4em;
 		font-weight: 100;
 	}
